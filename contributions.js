7083a7e5ff61143106b8b507808ef84e4a1b8361
TomoBossi <tomasbossi97@gmail.com>
2025-06-09 00:20:37 -0300

    first commit

---
+++ contributions.js
@@ -0,0 +1,146 @@
+const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Windows Phone|Opera Mini/i.test(navigator.userAgent);
+if (isMobile) {
+  document.getElementById("contributionsJustifyWrapper").style.display = "none";
+}
+
+const form = document.getElementById("contributionForm");
+const base64Input = document.getElementById("base64");
+const statusText = document.getElementById("statusText");
+const contributionsDiv = document.getElementById("contributions");
+
+getContributions();
+
+document.getElementById("customFileButton").addEventListener("click", () => {
+  document.getElementById("fileInput").click();
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const image = document.getElementById("fileInput").files[0];
+  const name = form.name.value;
+  const email = form.email.value;
+  const message = form.message.value;
+
+  statusText.textContent = "Enviando...";
+
+  if (image) {
+    if (!["image/jpeg", "image/png"].includes(image.type)) {
+      alert("Error: el archivo debe ser una imagen en alguno de los formatos soportados");
+      statusText.textContent = "";
+      return;
+    }
+    const reader = new FileReader();
+    reader.onload = () => {
+      const base64 = reader.result.split(",")[1]; // remove prefix
+      processAndSubmit(form, name, email, message, base64);
+    };
+    reader.onerror = () => {
+      alert("Error al leer el archivo");
+      statusText.textContent = "";
+    };
+    reader.readAsDataURL(image); // triggers onload
+  } else {
+    processAndSubmit(form, name, email, message);
+  }
+});
+
+function processAndSubmit(form, name, email, message, base64="") {
+  const formData = new URLSearchParams();
+  formData.append("name", name);
+  formData.append("email", email);
+  formData.append("message", message);
+  if (base64) formData.append("base64", base64);
+  fetch(form.action, {
+    method: "POST",
+    body: formData
+  })
+  .then(res => res.json())
+  .then(data => {
+    statusText.textContent = data.text;
+    if (data.status === 200) {
+      form.reset();
+      getContributions(true);
+    }
+    document.getElementById("fileName").textContent = "...";
+  })
+  .catch(err => {
+    console.error(err);
+    statusText.textContent = "Error al enviar";
+  });
+}
+
+function getContributions(submitted=false) {
+  contributionsDiv.innerHTML = "Cargando...";
+  fetch("https://script.google.com/macros/s/AKfycbyig4U_VSt_aEZ42UO3zCdvUxs59FiA6bdUhCU6o7rfV2dzy2WnjOvmIc4ppqn2iVYTyA/exec")
+    .then(response => response.json())
+    .then(contributions => {
+      contributionsDiv.innerHTML = submitted ? "<p>Tu aporte va a aparecer acá cuando sea aprobado :)</p>" : "";
+      if (!contributions.length && !submitted) {
+        contributionsDiv.innerHTML = "<p>Soyez le premier</p>";
+        return;
+      }
+
+      contributions.forEach(msg => {
+        const messageElement = document.createElement("div");
+        messageElement.className = "message";
+
+        const headerDiv = document.createElement("div");
+        headerDiv.className = "message-header";
+
+        const nameStrong = document.createElement("strong");
+        nameStrong.className = "message-name";
+        nameStrong.textContent = DOMPurify.sanitize(msg.name) || "---";
+
+        const dateSpan = document.createElement("span");
+        dateSpan.className = "message-date";
+        dateSpan.textContent = msg.date;
+
+        headerDiv.appendChild(nameStrong);
+        headerDiv.appendChild(dateSpan);
+
+        const emailEm = document.createElement("em");
+        emailEm.className = "message-email";
+        emailEm.textContent = DOMPurify.sanitize(msg.email) || "---";
+
+        const messageP = document.createElement("p");
+        messageP.className = "message-text";
+        messageP.textContent = DOMPurify.sanitize(msg.message) || "---";
+
+        messageElement.appendChild(headerDiv);
+        messageElement.appendChild(emailEm);
+        messageElement.appendChild(messageP);
+
+        if (msg.imageUrl) {
+          const imageTag = document.createElement("img");
+          imageTag.src = `https://drive.google.com/thumbnail?id=${msg.imageUrl.split("/").slice(-2, -1)[0]}&sz=w1000`;
+          messageElement.appendChild(imageTag);
+        }
+
+        if (msg.reply) {
+          const hr = document.createElement("hr");
+          hr.className = "dark-hr";
+          const reply = document.createElement("span");
+          reply.className = "message-text";
+          const arrow = document.createElement("strong");
+          arrow.className = "reply-arrow";
+          arrow.textContent = "> ";
+          reply.appendChild(arrow);
+          reply.append(msg.reply);
+          messageElement.appendChild(hr);
+          messageElement.appendChild(reply);
+        }
+
+        contributionsDiv.appendChild(messageElement);
+      });
+    })
+    .catch(err => {
+      contributionsDiv.innerHTML = "<p>Error al cargar los aportes</p>";
+      console.error(err);
+    });
+}
+
+document.getElementById("fileInput").addEventListener("change", function(event) {
+  const fileName = event.target.files[0] ? event.target.files[0].name : "...";
+  document.getElementById("fileName").textContent = fileName;
+});
---
+++ map.js
@@ -0,0 +1,117 @@
+let currentEntry;
+let currentImageIndex = 0;
+let currentOverlay = false;
+
+const map = L.map("map", {
+  center: [-34.57644572703189, -58.47911533206378],
+  zoom: 15,
+  attributionControl: false
+});
+
+L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+  subdomains: "abcd",
+  maxZoom: 19,
+  tileSize: 256,
+  detectRetina: true
+}).addTo(map);
+
+fetch("data.json")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(entry => {
+      const firstImage = entry.images[0];
+
+      const divIconMarkerContent = document.createElement("div");
+      divIconMarkerContent.className = "marker-content";
+      divIconMarkerContent.style.backgroundImage = `url("${firstImage.url.split('&sz=')[0] + '&sz=w100'}")`;
+      divIconMarkerContent.style.backgroundPosition = `center ${firstImage.thumbnailOffset}px`;
+
+      const divIcon = L.divIcon({
+        className: "custom-div-marker",
+        html: divIconMarkerContent,
+        iconSize: [40, 40],
+        iconAnchor: [20, 20]
+      });
+
+      const marker = L.marker([entry.latitude, entry.longitude], { icon: divIcon }).addTo(map);
+
+      marker.on("click", () => {
+        showEntry(entry);
+      });
+    });
+  });
+
+function showEntry(entry) {
+  currentEntry = entry;
+  currentImageIndex = 0;
+  currentOverlay = true;
+  showImage(currentEntry, currentImageIndex);
+
+  const info = document.getElementById("info");
+  info.innerHTML = `<strong>Lat:</strong> ${entry.latitude.toFixed(6)}<br><strong>Lon:</strong> ${entry.longitude.toFixed(6)}<br><br>${entry.description}<br><br><hr><br>`;
+
+  const overlay = document.getElementById("overlay");
+  overlay.style.display = "flex";
+}
+
+function showImage(entry, index) {
+  const image = entry.images[index];
+
+  const date = new Date(image.epoch * 1000)
+  let dateStr;
+  if (image.src === "Google Street View") {
+    dateStr = date.toLocaleString("es-AR", {
+      hour12: false,
+      year: "numeric",
+      month: "2-digit"
+    });
+  } else {
+    dateStr = date.toLocaleString("es-AR", {
+      hour12: false,
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit"
+    }).split(", ").join(" - ");
+  }
+
+  document.getElementById("imageIndexInfo").innerHTML = `Imagen ${index + 1}/${entry.images.length}`;
+  document.getElementById("imageImg").src = image.url;
+  const src = image.srcUrl ? `<a href="${image.srcUrl}" target="_blank">${image.src}</a>` : image.src
+  document.getElementById("imageText").innerHTML = `<strong>Fecha:</strong> ${dateStr}<br><strong>Fuente:</strong> ${src}`;
+}
+
+function closeOverlay() {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("imageImg").src = "";
+  currentOverlay = false;
+}
+document.getElementById("closeButton").setAttribute("onclick", "closeOverlay()");
+
+function nextImage(step=1) {
+  previousImageIndex = currentImageIndex;
+  currentImageIndex = Math.min(Math.max(0, currentImageIndex + step), currentEntry.images.length - 1);
+  if (previousImageIndex != currentImageIndex) {
+    document.getElementById("imageImg").src = "";
+    showImage(currentEntry, currentImageIndex);
+  }
+}
+document.getElementById("previousImageButton").setAttribute("onclick", "nextImage(-1)");
+document.getElementById("nextImageButton").setAttribute("onclick", "nextImage()");
+document.getElementById("imageImg").setAttribute("onclick", "nextImage()");
+
+document.addEventListener("keydown", (e) => {
+  switch (e.code) {
+    case "Escape":
+      closeOverlay();
+      break;
+    case "ArrowLeft":
+      nextImage(-1)
+      break;
+    case "ArrowRight":
+      nextImage()
+      break;
+  }
+});
